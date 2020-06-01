1782e962b6f0525a92be5cbd077bf1a1b1a6954a
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2020-06-01 13:01:51 +0500

    Fix rsync path

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rsync -avzP --exclude='.vscode' --exclude='.git' --exclude='.idea' --exclude='venv' --exclude='.gitignore' \
           --exclude='__pycache__' --exclude='sync.sh' \
-          -e ssh /home/vladimir/projects/diplom root@109.234.36.63:/home/projects
+          -e ssh /home/vladimir/projects/machinetools root@109.234.36.63:/home/projects

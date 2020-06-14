b5ab7f5cef2b588aed6120926cb324a1a922bd5b
Vladimir Karmanov <vladimir1998karmanov@gmail.com>
2020-06-14 15:18:01 +0500

    Add exclude folder static to rsync

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rsync -avzP --exclude='.vscode' --exclude='.git' --exclude='.idea' --exclude='venv' --exclude='.gitignore' \
-          --exclude='__pycache__' --exclude='sync.sh' --exclude='media' \
+          --exclude='__pycache__' --exclude='sync.sh' --exclude='media' --exclude='static' \
           -e ssh /home/vladimir/projects/machinetools root@109.234.36.63:/home/projects
